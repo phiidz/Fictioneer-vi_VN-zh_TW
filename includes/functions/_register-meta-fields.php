f8a890a6f969dff90493e92ddf380a208f12230c
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2025-11-22 22:19:58 +0100

    Optimize loop

--- includes/functions/_register-meta-fields.php
+++ includes/functions/_register-meta-fields.php
@@ -93,12 +93,13 @@ function fictioneer_rest_get_auth_callback_for_type( $type ) {
   }
 
   $keys_to_delete = [];
+  $allow_list = fictioneer_get_falsy_meta_allow_list();
 
   foreach ( $request['meta'] as $key => $value ) {
     if (
       empty( $value ) &&
       strpos( $key, 'fictioneer_' ) === 0 &&
-      ! in_array( $key, fictioneer_get_falsy_meta_allow_list() )
+      ! in_array( $key, $allow_list )
     ) {
       $keys_to_delete[] = $key;
     }
